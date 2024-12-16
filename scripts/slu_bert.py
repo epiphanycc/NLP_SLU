6fead50388c5f683e09017e74751590f42ea732d
epi <chfydl@126.com>
2024-12-16 16:12:39 +0800

    modify parameters and optimizer for better training.

--- scripts/slu_bert.py
+++ scripts/slu_bert.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
 import sys, os, time, gc, json
-from torch.optim import Adam
+from torch.optim import AdamW
 from transformers import BertTokenizer, BertForTokenClassification
 from torch.nn.utils.rnn import pad_sequence
-
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import StepLR
 install_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(install_path)
 from utils.args import init_args
@@ -39,16 +40,17 @@ model = BertForTokenClassification.from_pretrained(
     num_labels=Example.label_vocab.num_tags,
 ).to(device)
 
-# if args.testing:
-check_point = torch.load(open('model1.bin', 'rb'), map_location=device)
-model.load_state_dict(check_point['model'])
-print("Load saved model from root path")
+if args.testing:
+    check_point = torch.load(open('model1.bin', 'rb'), map_location=device)
+    model.load_state_dict(check_point['model'])
+    print("Load saved model from root path")
 
 def set_optimizer(model, args):
     params = [(n, p) for n, p in model.named_parameters() if p.requires_grad]
     grouped_params = [{'params': list(set([p for n, p in params]))}]
-    optimizer = Adam(grouped_params, lr=args.lr, weight_decay=0.01)
-    return optimizer
+    optimizer = AdamW(grouped_params, lr=args.lr, weight_decay= 0.01)
+    scheduler = StepLR(optimizer, step_size=8, gamma=0.5)
+    return optimizer, scheduler
 
 def encode_batch(dataset, tokenizer, max_len):
     input_ids, attention_masks, labels = [], [], []
@@ -167,58 +169,59 @@ def predict():
             ptr += 1
     json.dump(test_json, open(os.path.join(args.dataroot, 'prediction.json'), 'w', encoding='utf-8'), indent=4, ensure_ascii=False)
 
-# if not args.testing:
-#     writer = SummaryWriter(log_dir="runs/slu_experiment")
-#     num_training_steps = ((len(train_dataset) + args.batch_size - 1) // args.batch_size) * args.max_epoch
-#     print('Total training steps: %d' % (num_training_steps))
-#     optimizer = set_optimizer(model, args)
-#     nsamples, best_result = len(train_dataset), {'dev_acc': 0., 'dev_f1': 0.}
-#     train_index, step_size = np.arange(nsamples), args.batch_size
-#     print('Start training ......')
-#     loss_cnt = 0
-#     for i in range(args.max_epoch):
-#         start_time = time.time()
-#         epoch_loss = 0
-#         np.random.shuffle(train_index)
-#         model.train()
-#         count = 0
-#         for j in range(0, nsamples, step_size):
-#             cur_dataset = [train_dataset[k] for k in train_index[j: j + step_size]]
-#             input_ids, attention_masks, label_ids = encode_batch(cur_dataset, tokenizer, args.max_seq_len)
-#             input_ids, attention_masks, label_ids = input_ids.to(device), attention_masks.to(device), label_ids.to(device)
+if not args.testing:
+    writer = SummaryWriter(log_dir="runs/slu_experiment")
+    num_training_steps = ((len(train_dataset) + args.batch_size - 1) // args.batch_size) * args.max_epoch
+    print('Total training steps: %d' % (num_training_steps))
+    optimizer,scheduler = set_optimizer(model, args)
+    nsamples, best_result = len(train_dataset), {'dev_acc': 0., 'dev_f1': 0.}
+    train_index, step_size = np.arange(nsamples), args.batch_size
+    print('Start training ......')
+    loss_cnt = 0
+    for i in range(args.max_epoch):
+        start_time = time.time()
+        epoch_loss = 0
+        np.random.shuffle(train_index)
+        model.train()
+        count = 0
+        for j in range(0, nsamples, step_size):
+            cur_dataset = [train_dataset[k] for k in train_index[j: j + step_size]]
+            input_ids, attention_masks, label_ids = encode_batch(cur_dataset, tokenizer, args.max_seq_len)
+            input_ids, attention_masks, label_ids = input_ids.to(device), attention_masks.to(device), label_ids.to(device)
 
-#             outputs = model(input_ids, attention_mask=attention_masks, labels=label_ids)
-#             loss = outputs.loss
-#             epoch_loss += loss.item()
-#             loss.backward()
-#             writer.add_scalar('Loss/train', loss , loss_cnt)
-#             loss_cnt += 1
-#             optimizer.step()
-#             optimizer.zero_grad()
-#             count += 1
-#         print('Training: \tEpoch: %d\tTime: %.4f\tTraining Loss: %.4f' % (i, time.time() - start_time, epoch_loss / count))
-#         torch.cuda.empty_cache()
-#         gc.collect()
+            outputs = model(input_ids, attention_mask=attention_masks, labels=label_ids)
+            loss = outputs.loss
+            epoch_loss += loss.item()
+            loss.backward()
+            writer.add_scalar('Loss/train', loss , loss_cnt)
+            loss_cnt += 1
+            optimizer.step()
+            optimizer.zero_grad()
+            count += 1
+        print('Training: \tEpoch: %d\tTime: %.4f\tTraining Loss: %.4f' % (i, time.time() - start_time, epoch_loss / count))
+        torch.cuda.empty_cache()
+        gc.collect()
 
-#         start_time = time.time()
-#         metrics, dev_loss = decode('dev')
-#         dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
-#         writer.add_scalar('Loss/dev', dev_loss, i)
-#         writer.add_scalar('Accuracy/dev', dev_acc, i)
-#         writer.add_scalar('F1/dev', dev_fscore['fscore'], i)
-#         print('Evaluation: \tEpoch: %d\tTime: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, time.time() - start_time, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
-#         if dev_acc > best_result['dev_acc']:
-#             best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1'], best_result['iter'] = dev_loss, dev_acc, dev_fscore, i
-#             torch.save({
-#                 'epoch': i, 'model': model.state_dict(),
-#                 'optim': optimizer.state_dict(),
-#             }, open('model.bin', 'wb'))
-#             print('NEW BEST MODEL: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
+        start_time = time.time()
+        metrics, dev_loss = decode('dev')
+        scheduler.step()
+        dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
+        writer.add_scalar('Loss/dev', dev_loss, i)
+        writer.add_scalar('Accuracy/dev', dev_acc, i)
+        writer.add_scalar('F1/dev', dev_fscore['fscore'], i)
+        print('Evaluation: \tEpoch: %d\tTime: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)\tDev loss: %.4f' % (i, time.time() - start_time, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore'],dev_loss))
+        if dev_acc > best_result['dev_acc']:
+            best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1'], best_result['iter'] = dev_loss, dev_acc, dev_fscore, i
+            torch.save({
+                'epoch': i, 'model': model.state_dict(),
+                'optim': optimizer.state_dict(),
+            }, open('model.bin', 'wb'))
+            print('NEW BEST MODEL: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
 
-#     print('FINAL BEST RESULT: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.4f\tDev fscore(p/r/f): (%.4f/%.4f/%.4f)' % (best_result['iter'], best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1']['precision'], best_result['dev_f1']['recall'], best_result['dev_f1']['fscore']))
-# else:
-start_time = time.time()
-metrics, dev_loss = decode('dev')
-dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
-predict()
-print("Evaluation costs %.2fs ; Dev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)" % (time.time() - start_time, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
+    print('FINAL BEST RESULT: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.4f\tDev fscore(p/r/f): (%.4f/%.4f/%.4f)' % (best_result['iter'], best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1']['precision'], best_result['dev_f1']['recall'], best_result['dev_f1']['fscore']))
+else:
+    start_time = time.time()
+    metrics, dev_loss = decode('dev')
+    dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
+    predict()
+    print("Evaluation costs %.2fs ; Dev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)" % (time.time() - start_time, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
